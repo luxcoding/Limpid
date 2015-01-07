1560953480bc78e660b86060236e0c86ce6ec6ff
Georgiy <goshazvir@gmail.com>
2015-01-07 19:11:12 +0200

    asdasd

--- build/js/common.js
+++ build/js/common.js
@@ -14,7 +14,7 @@ head.ready(function() {
         slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', 'lastPage'],
         menu: '#menu', 
-        autoScrolling: false
+        // autoScrolling: false
       });
     }
 
