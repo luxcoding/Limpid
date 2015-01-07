2537a823e3d1942ac4c3b6c46a3aa34bd46513f0
Georgiy <goshazvir@gmail.com>
2015-01-07 14:09:03 +0200

    init

---
+++ src/js/init.js
@@ -0,0 +1,19 @@
+head.load(
+	"js/lib/jquery.min.js",
+	"js/lib/jquery.mousewheel.min.js",
+	"js/lib/jquery.event.move.js",
+	"js/lib/jquery-ui-1.10.4.min.js",
+	"js/lib/PxLoader.js",
+	"js/lib/jquery.loupe.min.js",
+	"js/lib/PxLoaderImage.js",
+	"js/lib/slick.js",
+	"js/lib/general.js",
+
+
+	// "js/lib/jquery-ui.min.js",
+	"js/lib/jquery.fullPage.js",
+	"js/lib/iscroll.js",
+	
+
+	"js/common.js"
+);
